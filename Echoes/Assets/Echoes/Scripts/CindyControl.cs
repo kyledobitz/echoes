2d6da6c602ea758adb954637ba98e435f4fdb1c5
kydo986 <kyledobitz@gmail.com>
2014-04-22 05:25:52 -0700

    Removed extra project files

--- Echoes/Assets/Echoes/Scripts/CindyControl.cs
+++ Echoes/Assets/Echoes/Scripts/CindyControl.cs
@@ -6,12 +6,12 @@ public class CindyControl : MonoBehaviour
 	public float maxSpeed = .5f;				// The fastest the player can travel in the x axis.
 	//public AudioClip[] jumpClips;			// Array of clips for when the player jumps.
 	//public AudioClip[] taunts;				// Array of clips for when the player taunts.
-	//private Animator anim;					// Reference to the player's animator component.
+	private Animator anim;					// Reference to the player's animator component.
 
 
 	void Awake()
 	{
-		//anim = GetComponent<Animator>();
+		anim = transform.Find("Model").GetComponent<Animator>();
 	}
 
 
@@ -26,10 +26,15 @@ public class CindyControl : MonoBehaviour
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
 
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		//anim.SetFloat("Speed", Mathf.Abs(h));
+		Vector2 velocity = (new Vector2 (h,v)).normalized*maxSpeed;
 
-		rigidbody2D.velocity = (new Vector2 (h,v)).normalized*maxSpeed;
+		// The Speed animator parameter is set to the absolute value of the horizontal input.
+		if (velocity.magnitude > maxSpeed * 0.20)
+			anim.SetBool ("Walking", true);
+		else
+			anim.SetBool ("Walking", false);
+
+		rigidbody2D.velocity = velocity;
 
 		// If the input is moving the player right and the player is facing left...
 		//if(h > 0 && !facingRight)
